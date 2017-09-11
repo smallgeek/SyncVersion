e3130ca6b3afa88f9d53d73ba74a88bbdd8c4420
smallgeek <smallgeek@outlook.jp>
2017-09-11 12:00:53 +0900

    Added CLI arguments parsing

--- src/SyncVersion/Program.fs
+++ src/SyncVersion/Program.fs
@@ -1,7 +1,33 @@
-﻿// F# の詳細については、http://fsharp.org を参照してください
-// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+﻿open Argu
+open System
+
+type Arguments =
+  | [<Mandatory>] Version of string
+  | [<Mandatory>] Solution of string
+with
+  interface IArgParserTemplate with
+    member x.Usage =
+      match x with
+      | Version _ -> "specify a version string."
+      | Solution _ -> "specify a solution file path."
+
+type Config = {
+  Version: string
+  Solution: string
+}
+
+let parseArguments (argv: string[]) =
+  let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+  let parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
+  let results = parser.Parse argv
+
+  { Version = results.GetResult <@ Version @>; Solution = results.GetResult <@ Solution @> }
 
 [<EntryPoint>]
 let main argv = 
-  printfn "%A" argv
-  0 // 整数の終了コードを返します
+  let result = parseArguments argv
+
+  printfn "%s" result.Version
+  printfn "%s" result.Solution
+
+  0
