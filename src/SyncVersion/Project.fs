dd52737ea4d106e7b492594f4db35fda143c1dcc
smallgeek <smallgeek@outlook.jp>
2017-09-11 17:10:41 +0900

    Added AndroidManifest detecting

--- src/SyncVersion/Project.fs
+++ src/SyncVersion/Project.fs
@@ -11,14 +11,19 @@
     let doc = XDocument.Load projectFile
 
     let project = doc.Element <| XName.Get("Project", ns)
-    let groups = project.Elements <| XName.Get("ItemGroup", ns) 
 
-    let files =
+    let assemblies =
+      let groups = project.Elements <| XName.Get("ItemGroup", ns) 
       groups 
       |> Seq.collect (fun g -> g.Elements <| XName.Get("Compile", ns))
       |> Seq.map (fun elm -> elm.Attribute <| XName.Get("Include"))
       |> Seq.filter (fun attr -> Path.GetFileNameWithoutExtension(attr.Value) = "AssemblyInfo")
       |> Seq.map (fun attr -> Path.Combine(projectDirectory, attr.Value))
 
-    files
+    let manifests =
+      let groups = project.Elements <| XName.Get("PropertyGroup", ns) 
+      groups 
+      |> Seq.collect (fun g -> g.Elements <| XName.Get("AndroidManifest", ns))
+      |> Seq.map (fun elm -> Path.Combine(projectDirectory, elm.Value))
 
+    Seq.append assemblies manifests
