42e21faa8ae818451e228af9153c44135105dc93
smallgeek <smallgeek@outlook.jp>
2017-09-11 16:58:33 +0900

    Shrinked replacing parameters

--- src/SyncVersion/Versioning.fs
+++ src/SyncVersion/Versioning.fs
@@ -3,22 +3,35 @@
   open System.Text.RegularExpressions
   open Cli
 
-  let csAssemblyVersionPattern = "^\[assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\]$"
-  let csAssemblyVersionFormat = "[assembly: AssemblyVersion(\"{0}\")]"
-  let csAssemblyFileVersionPattern = "^\[assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\]$"
-  let csAssemblyFileVersionFormat = "[assembly: AssemblyFileVersion(\"{0}\")]"
+  type Replacing = {
+    AssemblyVersionPattern : string
+    AssemblyVersionFormat : string
+    FileVersionPattern : string
+    FileVersionFormat : string
+  }
 
-  let fsAssemblyVersionPattern = "^\[\<assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\>\]$"
-  let fsAssemblyVersionFormat = "[<assembly: AssemblyVersion(\"{0}\")>]"
-  let fsAssemblyFileVersionPattern = "^\[\<assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\>\]$"
-  let fsAssemblyFileVersionFormat = "[<assembly: AssemblyFileVersion(\"{0}\")>]"
+  let csReplacing = {
+    AssemblyVersionPattern = "^\[assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\]$"
+    AssemblyVersionFormat = "[assembly: AssemblyVersion(\"{0}\")]"
+    FileVersionPattern = "^\[assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\]$"
+    FileVersionFormat = "[assembly: AssemblyFileVersion(\"{0}\")]"
+  }
 
-  let vbAssemblyVersionPattern = "^\<Assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\>$"
-  let vbAssemblyVersionFormat = "<Assembly: AssemblyVersion(\"{0}\")>"
-  let vbAssemblyFileVersionPattern = "^\<Assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\>$"
-  let vbAssemblyFileVersionFormat = "<Assembly: AssemblyFileVersion(\"{0}\")>"
+  let fsReplacing = {
+    AssemblyVersionPattern = "^\[\<assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\>\]$"
+    AssemblyVersionFormat = "[<assembly: AssemblyVersion(\"{0}\")>]"
+    FileVersionPattern = "^\[\<assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\>\]$"
+    FileVersionFormat = "[<assembly: AssemblyFileVersion(\"{0}\")>]"
+  }
 
-  let private updateCore(sourceFile: string) (version: Version) (pattern: string) (replaceFormat: string) (filePattern: string) (replaceFileFormat: string) =
+  let vbReplacing = {
+    AssemblyVersionPattern = "^\<Assembly: AssemblyVersion\(\"(?<version>.*?)\"\)\>$"
+    AssemblyVersionFormat = "<Assembly: AssemblyVersion(\"{0}\")>"
+    FileVersionPattern = "^\<Assembly: AssemblyFileVersion\(\"(?<version>.*?)\"\)\>$"
+    FileVersionFormat = "<Assembly: AssemblyFileVersion(\"{0}\")>"
+  }
+
+  let private updateCore(sourceFile: string) (version: Version) (replacing: Replacing) =
     let regexMatch s pattern (version: string) format =
       let m = Regex.Match(s, pattern)
       if m.Success then
@@ -29,8 +42,8 @@
     let lines = File.ReadAllLines(sourceFile)
     let newLines =
       lines 
-      |> Seq.map (fun s -> regexMatch s pattern version.Assembly replaceFormat)
-      |> Seq.map (fun s -> regexMatch s filePattern version.File replaceFileFormat)
+      |> Seq.map (fun s -> regexMatch s replacing.AssemblyVersionPattern version.Assembly replacing.AssemblyVersionFormat)
+      |> Seq.map (fun s -> regexMatch s replacing.FileVersionPattern version.File replacing.FileVersionFormat)
     
     File.WriteAllLines(sourceFile, newLines)
 
@@ -38,11 +51,10 @@
 
   let update(sourceFile: string) (version: Version) =
     
-    let update' = updateCore sourceFile version
     let ext = Path.GetExtension(sourceFile).ToLowerInvariant()
     
     match ext with
-    | ".cs" -> update' csAssemblyVersionPattern csAssemblyVersionFormat csAssemblyFileVersionPattern csAssemblyFileVersionFormat
-    | ".fs" -> update' fsAssemblyVersionPattern fsAssemblyVersionFormat fsAssemblyFileVersionPattern fsAssemblyFileVersionFormat
-    | ".vb" -> update' vbAssemblyVersionPattern vbAssemblyVersionFormat vbAssemblyFileVersionPattern vbAssemblyFileVersionFormat
+    | ".cs" -> updateCore sourceFile version csReplacing
+    | ".fs" -> updateCore sourceFile version fsReplacing
+    | ".vb" -> updateCore sourceFile version vbReplacing
     | _ -> ()
