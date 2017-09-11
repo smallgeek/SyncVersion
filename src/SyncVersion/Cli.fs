12c38443683cbedb7c99e2c1811a1daab5e4197f
smallgeek <smallgeek@outlook.jp>
2017-09-11 16:32:45 +0900

    Moved code related to arguments

---
+++ src/SyncVersion/Cli.fs
@@ -0,0 +1,26 @@
+﻿module Cli
+  open Argu
+  open System
+
+  type Config = {
+    Version: string
+    Solution: string
+  }
+
+  type Arguments =
+    | [<Mandatory>] Version of string
+    | [<Mandatory>] Solution of string
+  with
+    interface IArgParserTemplate with
+      member x.Usage =
+        match x with
+        | Version _ -> "specify a version string."
+        | Solution _ -> "specify a solution file path."
+
+  let parseArguments (argv: string[]) =
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
+    let results = parser.Parse argv
+
+    { Version = results.GetResult <@ Version @>; Solution = results.GetResult <@ Solution @> }
+
--- src/SyncVersion/Program.fs
+++ src/SyncVersion/Program.fs
@@ -1,27 +1,4 @@
-﻿open Argu
-open System
-
-type Arguments =
-  | [<Mandatory>] Version of string
-  | [<Mandatory>] Solution of string
-with
-  interface IArgParserTemplate with
-    member x.Usage =
-      match x with
-      | Version _ -> "specify a version string."
-      | Solution _ -> "specify a solution file path."
-
-type Config = {
-  Version: string
-  Solution: string
-}
-
-let parseArguments (argv: string[]) =
-  let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
-  let parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
-  let results = parser.Parse argv
-
-  { Version = results.GetResult <@ Version @>; Solution = results.GetResult <@ Solution @> }
+﻿open Cli
 
 [<EntryPoint>]
 let main argv = 
